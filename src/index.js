4fd124985e3913097e7220c859cb888d4c5e61e2
hongvinhmobile <lambiengcode@gmail.com>
2021-04-21 20:28:27 +0700

    [config] initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+import mongoose from "mongoose";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
+
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  server.applyMiddleware({ app });
+
+  await mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true
+  });
+
+  app.listen({ port: process.env.PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+  );
+};
+
+startServer();
